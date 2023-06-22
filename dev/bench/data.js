c2699143add8941c6db5bd7b47ad4a37cc77ea40
github-action-benchmark <github@users.noreply.github.com>
2023-06-22 21:37:05 +0000

    add Julia benchmark result (julia) benchmark result for 5adcc8de7495031e8e783e9f7e33ddd9b41c8464

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687460535024,
+  "lastUpdate": 1687469824482,
   "repoUrl": "https://github.com/navimakarov/LaplaceRedux.jl",
   "entries": {
     "Julia benchmark result": [
@@ -104,6 +104,38 @@ window.BENCHMARK_DATA = {
             "value": 679678860.5,
             "unit": "ns",
             "extra": "gctime=98279371.5\nmemory=2089649168\nallocs=2769052\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "navimakarov",
+            "username": "navimakarov"
+          },
+          "committer": {
+            "name": "navimakarov",
+            "username": "navimakarov"
+          },
+          "id": "5adcc8de7495031e8e783e9f7e33ddd9b41c8464",
+          "message": "Mlj interfacing tweaks",
+          "timestamp": "2023-05-31T20:38:56Z",
+          "url": "https://github.com/navimakarov/LaplaceRedux.jl/pull/40/commits/5adcc8de7495031e8e783e9f7e33ddd9b41c8464"
+        },
+        "date": 1687469820720,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "fit_la_batched",
+            "value": 374056720.5,
+            "unit": "ns",
+            "extra": "gctime=56554220\nmemory=1061381648\nallocs=1457837\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "fit_la_unbatched",
+            "value": 706937936,
+            "unit": "ns",
+            "extra": "gctime=101749954\nmemory=2089649168\nallocs=2769052\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
